617c9350186c45cda55eb5eb18db51f0ce107e59
Arnav Nagle <anagle@localhost.localdomain>
2022-09-20 14:15:45 -0700

    Link to model_structure and model_structure_uniprot table. Not working.

--- pdb.php
+++ pdb.php
@@ -31,7 +31,7 @@ $.cookie("lastBrowse", "<?php echo getLocalPDBURL($code,$scopReleaseID); ?>");
 
 // get basic info
 
-$path = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v2/".$name."-model_v2.cif.gz";
+$path = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v3/".$name."-model_v3.cif.gz";
 $awk_input = "zcat ".$path." | awk '$1 == \"_entity.pdbx_description\" || $1 == \"_ma_qa_metric_global.metric_value\" {\$1=\"\\x22\"$1\"\\x22:\"; print $0 \",\"'}";
 $af2_data = "{".substr(shell_exec($awk_input), 0, -2)."}";
 $decoded_af2_data = json_decode($af2_data);
@@ -42,13 +42,15 @@ print("<h2>AlphaFold entry ".$name."</h2>\n");
 print(getAF2Link($name,"View $name on AlphaFold Protein Structure Database site<br>\n"));
 print("<b>Description</b>: $description<br>\n");
 print("<b>Global pLDDT</b>: ".(is_null($global_metric_value) ? "N/A" : $global_metric_value)."<br>\n");
-$result = mysqli_query($mysqlLink, "select astral_domain.header, blast_log10_e, pct_identical, seq1_start, seq1_length, seq2_start, seq2_length from uniprot_seq, astral_seq_blast, astral_domain where 35522767 = uniprot_seq.uniprot_id and uniprot_seq.seq_id = astral_seq_blast.seq1_id and seq2_id = astral_domain.seq_id group by seq2_id order by blast_log10_e;");
+$query = "select astral_domain.header, astral_domain.sid, blast_log10_e, pct_identical, seq1_start, seq1_length, seq2_start, seq2_length from model_structure, model_structure_uniprot, uniprot_seq, astral_seq_blast, astral_domain where model_structure.cif_path = \"".$path."\" and model_structure.id = model_structure_uniprot.model_structure_id and model_structure_uniprot.uniprot_id = uniprot_seq.uniprot_id and uniprot_seq.seq_id = astral_seq_blast.seq1_id and seq2_id = astral_domain.seq_id and (astral_domain.style_id = 1 or astral_domain.style_id = 3) group by seq2_id order by blast_log10_e;";
+print($query);
+$result = mysqli_query($mysqlLink, $query);
 $n = mysqli_num_rows($result);
 $resdata="[";
 $seq_ids="[";
 for ($x = 0; $x < $n; $x++) {
 	$row = mysqli_fetch_assoc($result);
-	$seq_ids = $seq_ids.$row["astral_domain.header"].", ";
+	$seq_ids = $seq_ids.'"'.$row["sid"].'", ';
 	$seq1_end = $row["seq1_start"] + $row["seq1_length"];
 	$resdata = $resdata."[".$row["seq1_start"].", ".$seq1_end."], ";
 } 
@@ -57,7 +59,7 @@ $seq_ids = substr($seq_ids, 0, -2)."]";
 print("</div>\n");
 ?>
 
-<div style="height: 1000px">
+<div style="height: 700px">
   <canvas id="myChart"></canvas>
 </div>
 
