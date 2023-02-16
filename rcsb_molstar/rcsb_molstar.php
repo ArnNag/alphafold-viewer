cfee9e57e00d503237fe8071d6d9c6014b92503e
Arnav Nagle <anagle@localhost.localdomain>
2023-02-15 22:32:38 -0800

    abstract superposition to a function

--- rcsb_molstar/rcsb_molstar.php
+++ rcsb_molstar/rcsb_molstar.php
@@ -42,93 +42,104 @@ require_once ("../../common.php");
     </head>
     
    <body style="background-color:white;">
-        <?php
-            scopConnect();
-            $scopReleaseID = 0;
+<?php
+    scopConnect();
+    $scopReleaseID = 0;
 
-            if (isset($ver))
-                $scopReleaseID = lookupSCOPVersion(mysqli_real_escape_string($mysqlLink,$ver));
-            $maxSCOP = getLatestSCOPRelease();
-            if ($scopReleaseID==0)
-                $scopReleaseID = $maxSCOP;
-            $scopRelease = getSCOPVersion($scopReleaseID);
-            $dbName = getDBName($scopReleaseID);
-            if (($isProduction) && ($hackedURLs))
-                $verString = "ver=$scopRelease";
-            else
-                $verString = "?ver=$scopRelease";
-        
-            printCommonHeader("tab1",$scopReleaseID);
-            if (isset($oldURL))
-                warnOldURL($scopReleaseID);
+    if (isset($ver))
+	$scopReleaseID = lookupSCOPVersion(mysqli_real_escape_string($mysqlLink,$ver));
+    $maxSCOP = getLatestSCOPRelease();
+    if ($scopReleaseID==0)
+	$scopReleaseID = $maxSCOP;
+    $scopRelease = getSCOPVersion($scopReleaseID);
+    $dbName = getDBName($scopReleaseID);
+    if (($isProduction) && ($hackedURLs))
+	$verString = "ver=$scopRelease";
+    else
+	$verString = "?ver=$scopRelease";
 
-	    $result = mysqli_query($mysqlLink,"select * from scop_node where id = 1060");
-            $row = mysqli_fetch_row($result);
-		    
-            $row = mysqli_fetch_row($result);
+    printCommonHeader("tab1",$scopReleaseID);
+    if (isset($oldURL))
+	warnOldURL($scopReleaseID);
 
-	    $path = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v2/".$name;
+    $path = "/mnt/net/ipa.jmcnet/data/h/anagle/af2_human_v3/".$name."-model_v3.cif.gz";
+    /* $ln_path = "/af2_human_v3/".$name."-model_v3.cif.gz"; */
+    $ln_path = $name."-model_v3.cif";
 
-	    $awk_input = "zcat ".$path." | awk '$1 == \"_entity.pdbx_description\" || $1 == \"_ma_qa_metric_global.metric_value\" {\$1=\"\\x22\"$1\"\\x22:\"; print $0 \",\"'}";
-	    
-	    ?>
+    $awk_input = "zcat ".$path." | awk '$1 == \"_entity.pdbx_description\" || $1 == \"_ma_qa_metric_global.metric_value\" {\$1=\"\\x22\"$1\"\\x22:\"; print $0 \",\"'}";
+    
+    $query = "SELECT astral_domain.sid, translate_x, translate_y, translate_z, rotate_1_1, rotate_1_2, rotate_1_3, rotate_2_1, rotate_2_2, rotate_2_3, rotate_3_1, rotate_3_2, rotate_3_3 FROM model_structure JOIN model_vs_domain_structure_alignment AS m_vs_d ON model_structure.id = m_vs_d.model_id JOIN astral_domain ON m_vs_d.domain_id = astral_domain.id WHERE model_structure.cif_path = \"".$path."\";";
+
+$result = mysqli_query($mysqlLink, $query);
+$n = mysqli_num_rows($result);
+$sids=[];
+$rot_mats=[];
+
+for ($x = 0; $x < $n; $x++) {
+$row = mysqli_fetch_assoc($result);
+$sids[] = $row['sid'];
+$rot_mats[] = [floatval($row['rotate_1_1']), floatval($row['rotate_1_2']), floatval($row['rotate_1_3']),  0, floatval($row['rotate_2_1']), floatval($row['rotate_2_2']), floatval($row['rotate_2_3']), 0, floatval($row['rotate_3_1']), floatval($row['rotate_3_2']), floatval($row['rotate_3_3']), 0, floatval($row['translate_x']), floatval($row['translate_y']), floatval($row['translate_z']), 1];
+} 
+
+console_log(json_encode($rot_mats[0]));
+
+
+    console_log($ln_path);
+    ?>
 <style>
-    #app {
-        position: absolute;
-        left: 100px;
-        top: 100px;
-        width: 800px;
-        height: 600px;
-    }
+#app {
+position: absolute;
+left: 100px;
+top: 100px;
+width: 800px;
+height: 600px;
+}
 </style>
 <!-- 
-    molstar.js and .css are obtained from
-    - the folder build/viewer after cloning and building the molstar package 
-    - from the build/viewer folder in the Mol* NPM package
+molstar.js and .css are obtained from
+- the folder build/viewer after cloning and building the molstar package 
+- from the build/viewer folder in the Mol* NPM package
 -->
 <link rel="stylesheet" type="text/css" href="rcsb-molstar/build/dist/viewer/rcsb-molstar.css" />
 <script type="text/javascript" src="rcsb-molstar/build/dist/viewer/rcsb-molstar.js"></script>
 
 <div id="app"></div>
-        <script type="text/javascript">
-	    const viewer = new rcsbMolstar.Viewer('app', {
-                layoutShowControls: false,
-                viewportShowExpand: false,
-                viewportShowSelectionMode: true,
-                layoutShowSequence: true
-	    });
+<script type="text/javascript">
+    const ln_path = "<?php print($ln_path) ?>";
+    const viewer = new rcsbMolstar.Viewer('app', {
+	layoutShowControls: false,
+	viewportShowExpand: true,
+	viewportShowSelectionMode: true,
+	layoutShowSequence: true,
+	showWelcomeToast: false,
+	showStrucmotifSubmitControls: false,
+	showSuperpositionControls: false,
+	showStructureSourceControls: false,
+	showMeasurementsControls: false,
+	showStructureComponentControls: false,
+    });
 
-	    const idMat = [1, 0, 0, 0,
-		    0, 1, 0, 0, 
-		    0, 0, 1, 0,
-		    0, 0, 0, 1];
-	    const translateMat = [1, 0, 0, 0,
-		    0, 1, 0, 0, 
-		    0, 0, 1, 0,
-		    0, 40, 0, 1];
+    const idMat = [1, 0, 0, 0,
+	    0, 1, 0, 0, 
+	    0, 0, 1, 0,
+	    0, 0, 0, 1];
+    const translateMat = [1, 0, 0, 0,
+	    0, 1, 0, 0, 
+	    0, 0, 1, 0,
+	    0, 40, 0, 1];
 
-	    const rotMat = [
-							                             0.057,  0.997, -0.049, 0,
-							                             0.997, -0.059, -0.044, 0,
-							                            -0.047, -0.046, -0.998, 0,
-										     17.832, -16.343, 53.159, 1
-					                            ] ;
+    function superposeDomain(sid, mat) {
+	    const pdb_ln = 'pdbstyle/'.concat(sid.slice(2,4), '/', sid, '.ent');
 	    viewer.clear()
-		    .then(() => viewer.loadStructureFromUrl("d1av1b1.pdb", 'pdb', false))
-                    .then(() => viewer.loadStructureFromUrl("d1av1d1.pdb", 'pdb', false, { props: {  assemblyId: '1' }, matrix: rotMat}))
+                    .then(() => viewer.loadStructureFromUrl(pdb_ln, 'pdb', false))
+		    .then(() => viewer.loadStructureFromUrl(ln_path, 'mmcif', false, { props: {  assemblyId: '1' }, matrix: mat}))
 		    .then(() => viewer.resetCamera(0));
-	    
-	    
-                /* viewer.clear() */
-	    /*                     .then(function() { */
-			                        /* return viewer.loadStructureFromUrl({fileOrUrl: 'https://files.rcsb.org/download/3PQR.pdb', format: 'pdb', isBinary: false}, { props: { kind: 'standard', assemblyId: '1' } }) */
-					                    /* }) */
-					                        /* .then(function() { */
-						                            /* return viewer.loadPdbId('1u19', { props: {  assemblyId: '1' }, matrix: rotMat}) */
-			                        /* }) */
-                        /* .then(function() { */
-                        /* viewer.resetCamera(0) */
-                    /* }); */
+    }
+
+    const rotMat = <?php print(json_encode($rot_mats[0])) ?>;
+    const sid = <?php print(json_encode($sids[0])) ?>;
+    console.log(sid);
+    superposeDomain(sid, rotMat);
         </script>
 <?php 
 	    $af2_data = "{".substr(shell_exec($awk_input), 0, -2)."}";
@@ -137,11 +148,13 @@ require_once ("../../common.php");
 	    $global_metric_value = $decoded_af2_data->{'_ma_qa_metric_global.metric_value'};
 	    echo "description: ".$description;
 	    echo "global pLDDT: ".$global_metric_value;
+	    print($ln_path);
 	   
         done:
             // print("</div>\n");
             printCommonFooter($scopReleaseID);
             mysqli_close($mysqlLink);
         ?>
+	<button onclick="viewer.clear()">clear</button>
     </body>
 </html>
